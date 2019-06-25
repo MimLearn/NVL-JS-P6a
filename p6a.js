ba0cb723eb9e728ff8fcd0201cfb4582d707f1b6
MimLearn <51676988+MimLearn@users.noreply.github.com>
2019-06-25 11:02:42 +0200

    p6a.js

---
+++ p6a.js
@@ -0,0 +1,7 @@
+<script>
+var miNombreYapellidos = 'Maria de las Mercedes Crespo Jimenez';
+var edad = 25;
+var diasDeLaSemana = ['lunes','martes','miercoles','jueves','viernes','sabado','domingo'];
+var valorDeLaGravedad = 9.8;
+var tresPrimerosNumerosImpares = [1,3,5];
+</script>
